33463739de9e692395d6be3741a7283bc1b53102
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-07 07:01:41 +0300

    Create Nano_motors_test.ino

---
+++ CARrobo/CAR_nano_motors/Nano_motors_test.ino
@@ -0,0 +1,32 @@
+const int ENA = 10;
+const int in2 = 12;
+const int ENB = 9;
+const int in3 = 11;
+
+void setup() 
+{ 
+    pinMode(in2, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(in3, OUTPUT);
+} 
+  
+void loop() 
+{ 
+    digitalWrite(in3, 0);
+    analogWrite(ENB, 255);
+    digitalWrite(in2, 0);
+    analogWrite(ENA, 255); 
+    delay(2500);
+    analogWrite(ENB, 0);
+    analogWrite(ENA, 0);
+    delay(2500);
+    digitalWrite(in3, 1);
+    analogWrite(ENB, 255);
+    digitalWrite(in2, 1);
+    analogWrite(ENA, 255); 
+    delay(2500);
+    analogWrite(ENB, 0);
+    analogWrite(ENA, 0);
+    delay(2500);   
+}
