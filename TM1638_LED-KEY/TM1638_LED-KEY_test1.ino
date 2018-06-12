e2e5dc994c2e06b04fb15f0e67f645e30e36ed01
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-12 03:40:08 +0300

    Create TM1638_LED-KEY_test1.ino

---
+++ TM1638_LED-KEY/TM1638_LED-KEY_test1.ino
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+/* Подключение модуля: 
+ *  8pin - DIO(data) 
+ *  9pin - CLK(clock)
+ *  10pin - STB(strobe)
+ */
+#include <TM1638.h>
+TM1638 module(8, 9, 10);
+unsigned long a = 1;
+ 
+void setup()
+{
+}
+ 
+void loop()
+{
+    for (a=1; a<=100; a++)
+        {
+            module.setDisplayToDecNumber(a,0,false);
+            delay(100);
+        }
+    module.setDisplayToString("Complete");
+    delay(5000);
+}
