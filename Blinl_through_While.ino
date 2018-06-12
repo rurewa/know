2c1503c1157144c8614c6dbc9b8b1592a1c3acbf
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-12 08:38:36 +0300

    Create Blinl_through_While.ino

---
+++ Blinl_through_While.ino
@@ -0,0 +1,26 @@
+// Переменные unsigned long для расширенного хранения чисел
+unsigned long CurrentTime; 
+unsigned long HighTime;
+unsigned long LowTime;
+
+void setup()
+{
+  pinMode(13 , OUTPUT); // Настраиваем пин 13 как ВЫХОД
+}
+
+void loop()
+{
+  CurrentTime = millis();  // Присваиваем переменной per1 возвращённое количество времени с начала выполнения программы
+  HighTime = CurrentTime + 50; // Присваиваем переменной per2 сумму per1 + 50
+  LowTime = CurrentTime + 1000;  // Присваиваем переменной per3 сумму per1 + 500 
+  while (CurrentTime < HighTime) // Пока per1 < per2
+  {
+    digitalWrite(13, HIGH);  // Включаем светодиод
+    CurrentTime= millis(); // Снова присваиваем per1 возвращённое время с начала выполнения программы
+  }
+  while (CurrentTime < LowTime) // Пока per1 < per3
+  {
+    digitalWrite(13, LOW);  // Выключаем светодиод
+    CurrentTime = millis();  // Снова присваиваем per1 возвращённое время с начала выполнения программы
+  }
+}
