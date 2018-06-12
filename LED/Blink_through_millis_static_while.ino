ab0b4b586481a608c2e3524663246b7e158811f6
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-12 19:29:31 +0300

    Update Blink_through_millis_static_while.ino

--- LED/Blink_through_millis_static_while.ino
+++ LED/Blink_through_millis_static_while.ino
@@ -1,27 +1,27 @@
 // Мигаем светодиодом при помощи функции времени и оператора while
 // V 1.0
-unsigned long CurrentTime; 
-unsigned long HighTime;
-unsigned long LowTime;
-
+ 
 void setup()
 {
-  pinMode(13 , OUTPUT); // Настраиваем пин 13 как ВЫХОД
+  pinMode(9 , OUTPUT); 
 }
 
 void loop()
 {
-  CurrentTime = millis();  // Присваиваем переменной per1 возвращённое количество времени с начала выполнения программы
-  HighTime = CurrentTime + 50; // Присваиваем переменной per2 сумму per1 + 50
-  LowTime = CurrentTime + 1000;  // Присваиваем переменной per3 сумму per1 + 500 
-  while (CurrentTime < HighTime) // Пока per1 < per2
+  static unsigned long CurrentTime;
+  unsigned long HighTime;
+  unsigned long LowTime;
+  CurrentTime = millis();
+  HighTime = CurrentTime + 500;
+  LowTime = CurrentTime + 5000;
+  while (CurrentTime < HighTime)
   {
-    digitalWrite(13, HIGH);  // Включаем светодиод
-    CurrentTime= millis(); // Снова присваиваем per1 возвращённое время с начала выполнения программы
+    digitalWrite(9, HIGH);
+    CurrentTime= millis();
   }
-  while (CurrentTime < LowTime) // Пока per1 < per3
+  while (CurrentTime < LowTime)
   {
-    digitalWrite(13, LOW);  // Выключаем светодиод
-    CurrentTime = millis();  // Снова присваиваем per1 возвращённое время с начала выполнения программы
+    digitalWrite(9, LOW);
+    CurrentTime = millis();
   }
 }
