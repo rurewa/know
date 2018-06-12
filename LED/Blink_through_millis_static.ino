1cf3b9b6ba7332fb6b7f9742fa3cfd5f522a2f4c
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-12 19:06:35 +0300

    Create Blink_through_millis_static.ino

---
+++ LED/Blink_through_millis_static.ino
@@ -0,0 +1,21 @@
+// Blink_through_millis_static.ino
+
+void blink() 
+{
+  // static меняет время жизни переменной на время работы программы
+  static unsigned long blink_time = millis();
+  if (millis() >= blink_time) 
+  {       // настало ли время мигнуть
+    digitalWrite(9, !digitalRead(9));  // выключаем/включаем LED на 3,5 сек. 
+    blink_time = millis() + 100;      // задаём время следующего переключения
+  }
+}
+
+void setup() 
+{
+  pinMode(9, OUTPUT);
+}
+ 
+void loop() { 
+  blink();
+}
