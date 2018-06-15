95db2f41832c7bffa1216db374c6dabb845f0424
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-15 06:47:43 +0300

    Update CARrobo_kegelRing_from_China_1.ino

--- CARrobo/CARrobo_kegelRing_from_China_1.ino
+++ CARrobo/CARrobo_kegelRing_from_China_1.ino
@@ -1,5 +1,6 @@
 // For robocar from China
 // V 1.0 for kegelring end sumo
+#include <Arduino.h>
 #include <NewPing.h>
 const int ENA = 3;
 const int in1 = 5;
@@ -8,9 +9,9 @@ const int ENB = 9;
 const int in3 = 7;
 const int in4 = 8;
 const int SensorMiddle= 11;
-const int echo = 2;
 const int trig = 4;
-NewPing sonar (trig, echo, 200);
+const int echo = 13;
+NewPing sonar(trig, echo);
 
 void setup() 
 {
@@ -21,54 +22,44 @@ void setup()
     pinMode(in4, OUTPUT);
     pinMode(ENA, OUTPUT);
     pinMode(ENB, OUTPUT);
-    analogWrite(ENA, 155);
-    analogWrite(ENB, 155);
+    pinMode(trig, OUTPUT);
+    analogWrite(ENA, 125);
+    analogWrite(ENB, 125);
 }
 
 void loop() 
 {
-    /*Serial.println(sonar.ping_cm()); // Отладка
-    Serial.println(SM);
-    delay(150);*/
     int SM = digitalRead(SensorMiddle);
-    // Если расстояние до объекта < 20 см.
-    if (sonar.ping_cm() < 40) // Вперёд!
+    Serial.println(sonar.ping_cm()); // Отладка
+    //Serial.println(SM); // Отладка
+    if (sonar.ping_cm() >= 40)
+    {
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, LOW);
+        delay(400);
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
+        delay(250);
+    }
+    else if (SM == HIGH)
     {
         digitalWrite(in1, LOW);
         digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        delay(300);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
+        delay(1000);
+    }
+    else
+    {
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
         digitalWrite(in3, HIGH);
         digitalWrite(in4, LOW);
-        delay(1000);
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        delay(1000);
+        //delay(250);
     }
-    else if (SM == HIGH) // Назад!
-    {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        delay(500);
-    }
-    else // Иначе ищем цель
-    {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        delay(500);
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        delay(500);
-    }
+delay(150);    
 }
