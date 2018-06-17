c9998b578ad9d5946789dc5a6616eccdf545cc82
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-17 20:21:07 +0300

    Update CARrobo_kegelRing_for_Nano_motors.ino

--- CARrobo/CAR_nano_motors/CARrobo_kegelRing_for_Nano_motors.ino
+++ CARrobo/CAR_nano_motors/CARrobo_kegelRing_for_Nano_motors.ino
@@ -2,8 +2,6 @@
 // V 1.0 for kegelring end sumo
 #include <Arduino.h>
 #include <NewPing.h>
-#include <Servo.h>
-Servo myservo;
 const int ENA = 10;
 const int MotorRight = 12;
 const int ENB = 9; 
@@ -11,55 +9,50 @@ const int MotorLeft = 11;
 const int SensorMiddle= 7;
 const int echo = 3;
 const int trig = 5;
-NewPing sonar (trig, echo, 200);
+NewPing sonar (trig, echo, 400);
 
 void setup()
 {
   Serial.begin(9600);
-  //myservo.attach(13);
+  pinMode(trig, OUTPUT);
   pinMode(MotorRight, OUTPUT); 
   pinMode(MotorLeft, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
-  myservo.write(90);
 }
 
 void loop()
 {
     int SM = digitalRead(SensorMiddle);
     Serial.println(sonar.ping_cm()); // Отладка
-    Serial.println(SM);
-    // Если расстояние до объекта < 40 см.
-    if (sonar.ping_cm() > 0 && (sonar.ping_cm()) <= 40) // Вперёд!
+    //Serial.println(SM); // Отладка
+    // delay(150); // Отладка
+    // Если расстояние до объекта > 40 см.
+    if (sonar.ping_cm() > 40)
     {
-      digitalWrite(MotorRight, HIGH);
-      analogWrite(ENA, 155);
-      digitalWrite(MotorLeft, HIGH);
-      analogWrite(ENB, 155);
-      delay(3000);
-      digitalWrite(MotorRight, LOW);
-      analogWrite(ENA, 155);
-      digitalWrite(MotorLeft, LOW);
-      analogWrite(ENB, 155);
-      delay(3000);
+        analogWrite(ENA, 0);
+        analogWrite(ENB, 0);
+        delay(600);
+        digitalWrite(MotorRight, HIGH);
+        analogWrite(ENA, 135);
+        digitalWrite(MotorLeft, LOW);
+        analogWrite(ENB, 135);
+        delay(250);
     }
-    else if (SM == HIGH) // Назад!
+    else if (SM == HIGH)
     {
-      digitalWrite(MotorRight, LOW);
-      analogWrite(ENA, 155);
-      digitalWrite(MotorLeft, LOW);
-      analogWrite(ENB, 155);
-      delay(3000);
+        digitalWrite(MotorRight, LOW);
+        analogWrite(ENA, 135);
+        digitalWrite(MotorLeft, LOW);
+        analogWrite(ENB, 135);
+        delay(2000);
     }
-    else // Иначе ищем цель
+    else
     {
-      digitalWrite(MotorRight, LOW);
-      analogWrite(ENA, 155);
-      digitalWrite(MotorLeft, HIGH);
-      analogWrite(ENB, 155);
-      delay(200);
-      analogWrite(ENA, 0);
-      analogWrite(ENB, 0);
-      delay(200);
-    }
+        digitalWrite(MotorRight, HIGH);
+        analogWrite(ENA, 135);
+        digitalWrite(MotorLeft, HIGH);
+        analogWrite(ENB, 135);
+        delay(1000);
+    }  
 }
