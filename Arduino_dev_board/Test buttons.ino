074e548c9a7a97ab084271b5af119dcbafe6f763
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-22 06:33:54 +0300

    Update Test buttons.ino

--- Arduino_dev_board/Test buttons.ino
+++ Arduino_dev_board/Test buttons.ino
@@ -1,7 +1,6 @@
 // Arduino_dev_board v 1.0
 // V 1.0
 // Test buttons
-
 const int inPin8 = A7;
 const int inPin9 = A6;
 int val1;
@@ -10,8 +9,6 @@ int val2;
 void setup() 
 {
     Serial.begin(9600);
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
     pinMode(inPin8, INPUT);
     pinMode(inPin9, INPUT);
 }
@@ -21,13 +18,11 @@ void loop()
    val1 = analogRead(inPin8);
    val2 = analogRead(inPin9);
    Serial.print("AnalogOne: ");
-   Serial.print(val1, DEC);
+   Serial.print(val1);
    Serial.print("  AnalogTwo: ");
-   Serial.print(val2, DEC);
+   Serial.print(val2);
    val1 = analogRead(inPin8) > 700 ? LOW : HIGH;
    val2 = analogRead(inPin9) > 700 ? LOW : HIGH;
-   analogWrite(8, val1);
-   analogWrite(9, val2);
    Serial.print("  Button ONE: ");
    Serial.print(val1);
    Serial.print(" || ");
