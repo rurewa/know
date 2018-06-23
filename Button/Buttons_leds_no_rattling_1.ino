00afb4b4d118765d640b7e1fee0708b465a9d482
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-23 16:07:50 +0300

    Update Buttons_leds_no_rattling_1.ino

--- Button/Buttons_leds_no_rattling_1.ino
+++ Button/Buttons_leds_no_rattling_1.ino
@@ -1,22 +1,16 @@
-// Buttons leds no rattling
+// Buttons leds no rattling 1
 // V 1.0
 const int led1 = 9;
-const int led2 = 10;
 const int inPin1 = 7;
-const int inPin2 = 8;
 int button1 = 0;
-int button2 = 0;
 
 void setup()
 {
     pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
 }
 
 void loop()
 {
     button1 = digitalRead(inPin1);
-    button2 = digitalRead(inPin2);
     digitalWrite(led1, button1);
-    digitalWrite(led2, button2);
 }
