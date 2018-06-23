d660b6cc675df4e6588bc9e125786f2547cf0140
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-23 09:24:21 +0300

    Create PWM_led.ino

---
+++ Arduino_dev_board/PWM_led.ino
@@ -0,0 +1,24 @@
+// Arduino_dev_board v 1.0
+// V 1.0
+// PWM led
+const int led = 9;
+
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
+}
+
+void loop() 
+{
+   for (int i=10; i<256; i++)
+   {
+       analogWrite(led, i);
+       delay(10);
+   }
+   for (int i=255; i>=10; i--)
+   {
+       analogWrite(led, i);
+       delay(10);
+   }
+}
