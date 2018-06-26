b0fbbbad5df94ca2aa175d1e752e476f49781839
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-26 08:18:19 +0300

    Update Joystick_Two_Apart_motor_no_PWM_1.ino

--- Joystick/Joystick_Two_Apart_motor_no_PWM_1.ino
+++ Joystick/Joystick_Two_Apart_motor_no_PWM_1.ino
@@ -1,72 +1,76 @@
+// Joystick Two Apart motor no PWM 1
+// V 1.0
+int in1 = 4, in2 = 6;
+int in3 = 7, in4 = 8;
 int xValue, yValue;
-int in1 = 9;
-int in2 = 10;
-int in3 = 12;
-int in4 = 11;
-int xPin = A0;
-int yPin = A1;   
-
-void setup()
+ 
+void setup() 
 {
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  pinMode(xPin, INPUT);
-  pinMode(yPin, INPUT);
   Serial.begin(9600);
 }
-
-void loop()
+ 
+void loop() 
 {
-  xValue = map(analogRead(xPin), 0, 1024, 0, 252);
-  yValue = map(analogRead(yPin), 0, 1024, 0, 252);
-  Serial.print("X: ");
+  xValue = map(analogRead(A0), 0, 1024, -255, 255);
+  yValue = map(analogRead(A1), 0, 1024, -255, 255);
+  // Диагностика ----------------------------------
+  Serial.print("Xvalue: ");
   Serial.print(xValue); //выводим значение x
   Serial.print("\t"); //отступ
-  Serial.print("Y: ");
+  Serial.print("Yvalue: ");
   Serial.println(yValue); //выводим значение y
-  Serial.print("\t"); //отступ
-  delay(70);
-  if (xValue <= 100)
-    {
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-    }
-  else if (xValue > 101 && xValue <149)
-    {
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, LOW);
-    }
-  if (xValue >= 150)
-    {
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-    }
-  else if (xValue > 101 && xValue <149)
-    {
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, LOW);
-    }
-///////////////////////////////////////////
-  if (yValue <= 100)
-    {
-      digitalWrite(in3, HIGH);
-      digitalWrite(in4, LOW);
-    }
-  else if (yValue > 101 && yValue <149)
-    {
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, LOW);
-    }
-  if (yValue >= 150)
-    {
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, HIGH);
-    }
-  else if (yValue > 101 && yValue <149)
-    {
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, LOW);
-    }
+  Serial.print("\t");
+  delay(100);
+  // ----------------------------------------------
+// Нейтральное положение - моторы стоп
+if (xValue >= -10 && xValue <= 10)  
+{
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    Serial.print("X stop\t");
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    Serial.print("Y stop\t");
+}
+if (xValue > 11) // Джойстик по Х вверх
+{
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    Serial.print("X up\t");
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    Serial.print("Y up\t");
+}
+if (xValue < -11) // Джойстик по Х вниз  
+{
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    Serial.print("X down\t");
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    Serial.print("Y down\t");
+}
+if (yValue > 11) // Джойстик по Y вправо 
+{
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    Serial.print("X left\t");
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    Serial.print("Y left\t");
+}
+if (yValue < -11) // Джойстик по Y влево  
+{
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    Serial.print("X right\t");
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    Serial.print("Y right\t");
+}
+  Serial.println("");
 }
