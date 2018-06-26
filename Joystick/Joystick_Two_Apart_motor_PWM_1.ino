ac48d51deb12acec99615a331ac683d1c5e516b0
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-26 08:08:40 +0300

    Update Joystick_Two_Apart_motor_PWM_1.ino

--- Joystick/Joystick_Two_Apart_motor_PWM_1.ino
+++ Joystick/Joystick_Two_Apart_motor_PWM_1.ino
@@ -1,69 +1,87 @@
-#define ENA 3
-#define in1 9
-#define in2 10
-#define ENB 5
-#define in3 12
-#define in4 11
-int valX, valY;
+#include <Arduino.h>
+// Joystick Two Apart motor PWM 1
+// V 1.0
+int enA = 3, in1 = 4, in2 = 6;
+int enB = 5, in3 = 7,  in4 = 8;
+int xValue, yValue;
  
 void setup() 
 {
-  Serial.begin(9600);  
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+  Serial.begin(9600);
 }
  
 void loop() 
 {
-  valX = map(analogRead(A0), 0, 1024, -255, 255);
-  valY = map(analogRead(A1), 0, 1024, -255, 255);
- 
-  Serial.print(valX);    //выводим значение x
-  Serial.print("\t");      //отступ
-  Serial.print(valY);    //выводим значение y
+  xValue = map(analogRead(A0), 0, 1024, -255, 255);
+  yValue = map(analogRead(A1), 0, 1024, -255, 255);
+  // Диагностика ----------------------------------
+  Serial.print("Xvalue: ");
+  Serial.print(xValue); //выводим значение x
+  Serial.print("\t"); //отступ
+  Serial.print("Yvalue: ");
+  Serial.println(yValue); //выводим значение y
   Serial.print("\t");
-  if (valX >= -10 && valX <= 10)  
-  {
+  delay(100);
+  // ----------------------------------------------
+// Нейтральное положение - моторы стоп
+if (xValue >= -10 && xValue <= 10)  
+{
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
     Serial.print("X stop\t");
-  }
-  if (valX > 11)  {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    analogWrite(ENA, valX);
-    Serial.print("X up\t");
-  }
-  if (valX < -11)  {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    analogWrite(ENA, -valX);
-    Serial.print("X down\t");
-  }
- 
-  if (valY >= -10 && valY <= 10)   
-  {
     digitalWrite(in3, LOW);
     digitalWrite(in4, LOW);
     Serial.print("Y stop\t");
-  }
-  if (valY > 11)  
-  {
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-    analogWrite(ENB, valY);
-    Serial.print("Y up\t");
-  }
-  if (valY < -11)  
-  {
+}
+if (xValue > 11) // Джойстик по Х вверх
+{
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    analogWrite(enA, xValue);
+    Serial.print("X up\t");
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-    analogWrite(ENB, -valY);
+    analogWrite(enB, xValue);
+    Serial.print("Y up\t");
+}
+if (xValue < -11) // Джойстик по Х вниз  
+{
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    analogWrite(enA, -xValue);
+    Serial.print("X down\t");
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, -xValue);
     Serial.print("Y down\t");
-  }
+}
+if (yValue > 11) // Джойстик по Y вправо 
+{
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    analogWrite(enA, yValue);
+    Serial.print("X left\t");
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    analogWrite(enB, yValue);
+    Serial.print("Y left\t");
+}
+if (yValue < -11) // Джойстик по Y влево  
+{
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    analogWrite(enA, -yValue);
+    Serial.print("X right\t");
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, -yValue);
+    Serial.print("Y right\t");
+}
   Serial.println("");
 }
