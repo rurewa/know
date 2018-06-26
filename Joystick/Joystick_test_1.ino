f1a0c9a904d18610a48f7235121a17479191eb50
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-26 08:53:47 +0300

    Update Joystick_test_1.ino

--- Joystick/Joystick_test_1.ino
+++ Joystick/Joystick_test_1.ino
@@ -1,37 +1,33 @@
-const int VRx = 0;  // Ось X подключена к А0
-const int VRy = 1;  // Ось Y подключена к А1
-const int SWz = 2;  // Ось Z (кнопка) подключена к пин 2
-// Объявляем переменные для хранения значений
-int valueX; // Для VRx
-int valueY; // Для VRy
-int buttonZ = 0;  // Для SWz кнопки
+#include <Arduino.h>
+// Joystick test
+// V 1.0
 
-void setup()  // Старт и настройка программы
+void setup()
 {
   // Скорость передачи данных в монитор порта
   Serial.begin(9600);
   // Настраиваем пин 2 как ВХОД с подключением 
   // встроенного в Arduino подтягивающего резистора
-  pinMode(SWz, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
 }
 
-void loop() // Основная программа. Цикл
+void loop()
 {
   // Считываем в переменную значения с А0
-  valueX = analogRead(VRx); 
+  int valueX = analogRead(0); 
   // Посылаем сообщение в монитор порта
   Serial.print("X: ");
-  Serial.print(valueX, DEC);
+  Serial.print(valueX);
   // Считываем в переменную значения с А1
-  valueY = analogRead(VRy);
+  int valueY = analogRead(1);
   // Посылаем сообщение в монитор порта
   Serial.print(" | Y: ");
-  Serial.print(valueY, DEC);
+  Serial.print(valueY);
   // Считываем в переменную значения с пин 2
-  buttonZ = digitalRead(SWz);
+  int buttonZ = digitalRead(2);
   buttonZ = buttonZ ^ 1;
   // Посылаем сообщение в монитор порта
   Serial.print(" | SW: ");
-  Serial.println(buttonZ, DEC);
-  delay(1500);  // Задержка 1,5 секунды
+  Serial.println(buttonZ);
+  delay(300);  // Задержка
 }
