d80509cabd6f3991924b336e769cf387f86726f8
rurewa <34348805+rurewa@users.noreply.github.com>
2018-06-29 05:17:13 +0300

    Create Motor_PWM.ino

---
+++ Arduino_dev_board/Motor_PWM.ino
@@ -0,0 +1,26 @@
+// Arduino_dev_board v 1.0
+// Motor PWM
+// V 1.0
+// константы
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(3, OUTPUT);
+}
+
+void loop() 
+{
+  
+  for (int i=10; i<=256; i++)
+  {
+    analogWrite(3, i);
+    delay(10);
+  }
+  for (int i=255; i>=10; i--)
+  {
+    analogWrite(3, i);
+    delay(10);
+  }
+  delay(2000);
+}
