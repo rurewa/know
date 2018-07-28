b1cd79aa1050cac6e2367f4f80908429ad385176
rurewa <34348805+rurewa@users.noreply.github.com>
2018-07-28 21:52:44 +0300

    Create Test buttons_&_leds.ino

---
+++ Arduino_dev_board/Version_2/Test buttons_&_leds.ino
@@ -0,0 +1,21 @@
+// Arduino_dev_board v 2.0
+// V 1.0
+// Test buttons & leds
+
+void setup()
+{
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(8,digitalRead(13)); // led 8, button 13
+  digitalWrite(9,digitalRead(16)); // led 9, button 16
+  digitalWrite(10,analogRead(17)); // led10, button 17
+  digitalWrite(11,analogRead(17)); // led 11
+  digitalWrite(12,analogRead(17)); // led 12
+}
