0b95a5b59d2eeb3e4c2e39e4e570a042a86e0c10
rurewa <34348805+rurewa@users.noreply.github.com>
2018-08-02 09:49:17 +0300

    Create Debounсe_button_1.ino

---
+++ Arduino_dev_board/Debounсe_button_1.ino
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+// Debounсe button for Arduino dev board 2.0
+// V 1.0
+/*
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Debounce
+*/
+
+const int buttonPin = 13;
+const int ledPin = 8;
+// Переменные состояния светодиода и кнопки
+int ledState = LOW;         // Текущее состоние светодиода
+int buttonState;             // Текущее состояние кнопки
+int lastButtonState = LOW;   // Предыдущее состояние кнопки
+// Переменные таймера и задержки
+unsigned long lastDebounceTime = 0;  // Первоначальное значение таймера
+unsigned long debounceDelay = 50;    // Значение переменной отладки
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+}
+
+void loop()
+{
+  // Считываем состояние кнопки
+  int reading = digitalRead(buttonPin);
+  // Если текущее состояние кнопки не равно предыдущему
+  if (reading != lastButtonState)
+  {
+    // Сбрасываем таймер
+    lastDebounceTime = millis();
+  }
+
+  // Если таймер меньше значения отладки
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    // Если состояние кнопки изменилось
+    if (reading != buttonState)
+    {
+      buttonState = reading;
+      // Если состояние кнопки HIGH
+      if (buttonState == HIGH)
+      {
+        ledState = !ledState; // Инверсия состояния светодиода
+      }
+    }
+  }
+  // Устанавливаем состояние светодиода
+  digitalWrite(ledPin, ledState);
+  // Сохраняем значение
+  lastButtonState = reading;
+}
