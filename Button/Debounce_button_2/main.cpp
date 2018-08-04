60892ab8a12f4111dca1f7a7606d2cd6aa7b1568
rurewa <34348805+rurewa@users.noreply.github.com>
2018-08-04 09:08:23 +0300

    Create main.cpp

---
+++ Button/Debounce_button_2/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <Button.h>
+// Debounse button
+// V 1.0
+
+Button button;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  button.checkState();
+}
