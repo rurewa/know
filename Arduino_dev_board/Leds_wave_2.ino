94aa4019e2c24c8bed339695dc8d26810b137e25
rurewa <34348805+rurewa@users.noreply.github.com>
2018-10-20 07:34:59 +0300

    Update Leds_wave_2.ino

--- Arduino_dev_board/Leds_wave_2.ino
+++ Arduino_dev_board/Leds_wave_2.ino
@@ -3,12 +3,7 @@
 // Led wave 2
 void setup() 
 {
-  /*pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);*/
-  for (int thisPin = 8; thisPin <= 12; thisPin++)
+  for (int thisPin = 8; thisPin <= 12; ++thisPin)
     {
       pinMode(thisPin, OUTPUT);
     }
@@ -16,7 +11,7 @@ void setup()
 
 void loop() 
 {
-  for (int pin = 8; pin <= 12; pin++)
+  for (int pin = 8; pin <= 12; ++pin)
   {
     digitalWrite(pin, HIGH);
     delay(100);
