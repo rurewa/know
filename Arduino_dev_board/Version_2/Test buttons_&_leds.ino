09fb687c7ad6df6cc4facfbc3019e0f42c51d840
rurewa <34348805+rurewa@users.noreply.github.com>
2018-12-02 07:23:14 +0300

    Update Test buttons_&_leds.ino

--- Arduino_dev_board/Version_2/Test buttons_&_leds.ino
+++ Arduino_dev_board/Version_2/Test buttons_&_leds.ino
@@ -15,7 +15,7 @@ void loop()
 {
   digitalWrite(8,digitalRead(13)); // led 8, button 13
   digitalWrite(9,digitalRead(16)); // led 9, button 16
-  digitalWrite(10,analogRead(17)); // led10, button 17
-  digitalWrite(11,analogRead(17)); // led 11
-  digitalWrite(12,analogRead(17)); // led 12
+  digitalWrite(10,digitalRead(17)); // led10, button 17
+  digitalWrite(11,digitalRead(17)); // led 11
+  digitalWrite(12,digitalRead(17)); // led 12
 }
