a237806544e1c5249d65df66cec368b418684e8f
Unknown <rise.sp@gmail.com>
2019-03-17 08:43:46 +0300

    Millis_class_1

---
+++ LED/millis_class_1.ino
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+class TimerOne {
+    private:
+        unsigned long currentMillis = 0; // Текущее время
+        unsigned long previousMillis = 0; // Предыдущее время
+        bool ready = false; // Флаг
+
+    public:
+        unsigned int _interval; // Интервал
+        TimerOne(unsigned int interval) {
+            _interval = interval; //
+        }
+
+        bool callback() { // Конструктор
+                currentMillis = millis();
+                // Если millis() минус 0 и больше или равно 0, выполняем:
+                if (currentMillis - previousMillis >= _interval) {
+                    previousMillis = currentMillis; // Предыдущее время меняется на текущее
+                    return ready = !ready; // Инвертируем флаг и возвращаем флаг
+                }
+                else { ready = false; return ready; } // Иначе возвращаем значение флага
+
+        }
+};
+
+TimerOne Test5000ms(5000);
+TimerOne Test1000ms(1000);
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+    if (Test5000ms.callback()) {
+      digitalWrite(13, HIGH);
+      if (Test1000ms.callback()) {
+      digitalWrite(13, LOW);
+      }
+    }
+}
