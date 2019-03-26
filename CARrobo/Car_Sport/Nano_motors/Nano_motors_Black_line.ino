c826d1bd5a3de194cfdb01e616c0fb27e6048d1f
Unknown <rise.sp@gmail.com>
2019-03-26 10:27:48 +0300

    Update Nano motors. Fix space

--- CARrobo/Car_Sport/Nano_motors/Nano_motors_Black_line.ino
+++ CARrobo/Car_Sport/Nano_motors/Nano_motors_Black_line.ino
@@ -67,7 +67,7 @@ void loop()
     else if((!SL) && (SM) && (SR)) { _right(); Serial.print("RIGHT: "); } // Если M и L на белом - вправо!
     else if((SL) && (SM) && (!SR)) { _left(); Serial.print("LEFT: "); } // Если M и L на чёрном - влево!
     else if((!SL) && (SM) && (SR)) { _right(); Serial.print("RIGHT: "); } // Если M и R на чёрном - вправо!
-    else if((SL) && (SM) && (SR)) { _foward_left(); Serial.print("FOWARD & LEFT: ");} // Если все на чёрном - вперёд и влево!
+    else if((SL) && (SM) && (SR)) { _foward_left(); Serial.print("FOWARD & LEFT: "); } // Если все на чёрном - вперёд и влево!
     else { _foward_right(); Serial.print("FOWARD & RIGHT: "); } // Если все на белом - вперёд и вправо!
   }
   while(true);
