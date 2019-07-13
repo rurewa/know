bff96ad8a75252bd68eab98106718e331403906f
rurewa <34348805+rurewa@users.noreply.github.com>
2019-07-13 10:19:33 +0300

    Update RGB_if_switch_case_button_set.cpp

--- Arduino_dev_board/Version_2/RGB_if_switch_case_button_set.cpp
+++ Arduino_dev_board/Version_2/RGB_if_switch_case_button_set.cpp
@@ -1,80 +1,88 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
-// RGB - светильник, управляемый одной кнопкой c эффектом триггера. Для Arduino Dev Board
-// V 1.0
+// RGB светильник с счётчиком кратного нажатию приращения, ограничителем по количеству приращений
+// и простым "антидребегом"
+// V 1.1
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 #include <Arduino.h>
-
 const int BLED = 9;
 const int GLED = 10;
 const int RLED = 11;
-
-int ledMode =  0; // Режимы для выбора цвета
-// bool buttonState = 0; // Состояние кнопки (нажата/не нажата)
-
-void colourRed();
-void colourGreen();
-void colourBlue();
-void colourWhite();
-void colourOff();
+int MODE = 0; // Значение статуса режима RGB светодиода
+const int COUNT = 4; // Предельное число режимов
+bool currentState = false; // Текущее состояние кнопки
+bool lastState = false; // Флаг предыдущего состояния кнопки. Служит для синхронизации между подпрограммами
+// Прототипы функций
+void blueMode();
+void greenMode();
+void redMode();
+void offMode();
 
 void setup() {
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  Serial.begin(9600);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  bool buttonState = digitalRead(13);
-  if (buttonState == true) {
-    // Счётчик увеличивает ledMode и обнуляет, когда значение достигает 3
-    if (ledMode++ >= 3) { ledMode = 0; } // Счётчик и ограничитель
-      switch (ledMode)
+  currentState = digitalRead(17); // Опрашиваем кнопку
+  if(currentState == true && lastState == false) // Если текущее состояние отличается от предыдущего
+    {
+      delay(5); // Ждём немного (для стабилизации кнопки после эффекта "дребезга")
+      currentState = digitalRead(17); // Снова опрашиваем кнопку для подтверждения
+      if(currentState != lastState) // Если текущее состояние подтверждается
       {
-        case 1:
-          colourRed();
-        break;
-        case 2:
-          colourGreen();
-          break;
-        case 3:
-          colourBlue();
-        break;
-        default:
-          colourOff();
-        break;
-     }
-  Serial.println(ledMode); // Для диагностики
-  }
+        while (MODE++ >= COUNT) { MODE = 0; } // Запускаем счётчик с ограничителем
+        // Предыдущее состояние уже стало true! Поэтому меняем значение, чтобы больше не повторялось
+        lastState = currentState;
+        Serial.println(MODE);
+        switch(MODE)
+        {
+          case 1:
+            blueMode();
+            break;
+          case 2:
+            greenMode();
+            break;
+          case 3:
+            redMode();
+            break;
+          default:
+            offMode();
+            break;
+        }
+      }
+    }
+  // Если текущее состояние кнопки false, то предыдущее состояние, по логике, было true!
+  if (currentState == false) { lastState = currentState; }
 }
 
-void colourRed() { // Красный
-  digitalWrite(BLED, LOW);
-  digitalWrite(GLED, LOW);
-  digitalWrite(RLED, HIGH);
+void blueMode()
+{
+    digitalWrite(RLED, LOW);
+    digitalWrite(GLED, LOW);
+    digitalWrite(BLED, HIGH);
 }
 
-void colourGreen() { // Зелёный
-  digitalWrite(BLED, LOW);
-  digitalWrite(GLED, HIGH);
+void greenMode()
+{
+    digitalWrite(RLED, LOW);
+    digitalWrite(GLED, HIGH);
+    digitalWrite(BLED, LOW);
+}
+
+void redMode()
+{
+    digitalWrite(RLED, HIGH);
+    digitalWrite(GLED, LOW);
+    digitalWrite(BLED, LOW);
+}
+
+void offMode()
+{
   digitalWrite(RLED, LOW);
-}
-
-void colourBlue() { // Голубой
-  digitalWrite(BLED, HIGH);
   digitalWrite(GLED, LOW);
-  digitalWrite(RLED, LOW);
-}
-
-void colourWhite() { // Белый
-  digitalWrite(BLED, HIGH);
-  digitalWrite(GLED, HIGH);
-  digitalWrite(RLED, HIGH);
-}
-void colourOff() { //  Выключить все цвета
   digitalWrite(BLED, LOW);
-  digitalWrite(GLED, LOW);
-  digitalWrite(RLED, LOW);
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 // END FILE
