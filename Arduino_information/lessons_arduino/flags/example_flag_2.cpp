a4f2c66a50264a00eb8b4ea157dd8d4da5fe686a
Alex <rise.sp@gmail.com>
2019-07-17 11:47:47 +0300

    Update sketch example flag 2

--- Arduino_information/lessons_arduino/flags/example_flag_2.cpp
+++ Arduino_information/lessons_arduino/flags/example_flag_2.cpp
@@ -7,69 +7,76 @@
 bool foundStart = false; // Флаг начала строительства фундамента
 bool foundEnd = false; // Флаг завершения строительства фундамента
 // Стены
-bool wallsStart = false; // Флаг начала строительства стен
-bool wallsEnd = false; // Флаг завершения строительства стен
+bool wallStart = false; // Флаг начала строительства стен
+bool wallEnd = false; // Флаг завершения строительства стен
 
 // Крыша
-bool roofStart = false; // Флаг завершения строительства крыши
+bool roofStart = false; // Флаг начала строительства крыши
 bool roofEnd =  false; // Флаг завершения строительства крыши
 
-bool flag = true; // Флаг синхронизации
+// Флаги синхронизации
+bool flagFound = true;
+bool flagWalls = true;
+bool flagRoof = true;
 
 void printFound();
+void printWalls();
+void printRoof();
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-  // Начали строительство фундамента
+  // Начало строительства фундамента
   foundStart = true; // Поменяли значение флага, т.к. начали строительство
   // Проверяем, начато ли строительство?
-  if(foundStart == true) {
-    // Проверяем, завершено ли строительство? Если оно не завершено..
-    while(foundEnd != foundStart) {
+  if (foundStart == true) {
+     // Проверяем, завершено ли строительство? Если оно не завершено..
+     while(foundEnd != foundStart) {
         foundEnd = true; // Завершаем. Теперь флаги синхронизированы
         printFound();
-      }
+    }
   }
   // Проверка завершения строительства фундамента
-  if(foundStart == false) {
-    while(flag == true) {
-        flag = false; // Синхронизируем флаги
+  if (foundStart == false) {
+     while (flagFound == true) {
+        flagFound = false; // Синхронизируем флаги
         printFound();
-      }
+     }
   }
 
-  // Начали строительство стен
-  /*wallsStart = foundEnd; // Проверка, если фундамент построен, что начинаем строительсво
-  if (wallsStart == !wallsEnd) // Проверяем, начато ли строительство?
-    {
-    if (wallsEnd != wallsStart) // Проверяем, завершено ли строительство? Если оно не завершено..
-      {
-        wallsEnd = wallsStart;
-        Serial.println("");
-        Serial.print("wallsStart: ");
-        Serial.print(wallsStart);
-        Serial.print("  ");
-        Serial.print("wallsEnd: ");
-        Serial.print(wallsEnd);
-      }
+  // Начало строительства стен
+  wallStart = foundEnd;
+  if (wallStart == true) {
+     while (wallEnd != wallStart) {
+       wallEnd = true;
+       printWalls();
     }
-  roofStart = wallsEnd;
-  if (roofStart == !roofEnd)
-    {
-    if (roofEnd != roofStart)
-      {
-        roofEnd = roofStart;
-        Serial.println("");
-        Serial.print("roofStart: ");
-        Serial.print(roofStart);
-        Serial.print("  ");
-        Serial.print("roofEnd: ");
-        Serial.print(roofEnd);
-      }
-    }*/
+  }
+  // Проверка завершения строительства стен
+  if (wallStart == false) {
+     while (flagWalls == true) {
+       flagWalls =  false;
+       printWalls();
+    }
+  }
+
+  // Начало строительства крыши
+  roofStart  = wallEnd;
+  if (roofStart == true) {
+     while (roofEnd != roofStart) {
+       roofEnd = true;
+       printRoof();
+    }
+  }
+  // Проверка завершения строительства стен
+  if (roofStart == false) {
+     while (flagRoof == true) {
+       flagRoof =  false;
+       printRoof();
+    }
+  }
 }
 
 void printFound() {
@@ -78,6 +85,24 @@ void printFound() {
   Serial.print("  ");
   Serial.print("foundEnd: ");
   Serial.print(foundEnd);
+}
+
+void printWalls() {
+  Serial.println("  ");
+  Serial.print("wallStart: ");
+  Serial.print(wallStart);
+  Serial.print("  ");
+  Serial.print("wallllEnd: ");
+  Serial.print(wallEnd);
+}
+
+void printRoof() {
+  Serial.println("  ");
+  Serial.print("roofStart: ");
+  Serial.print(roofStart);
+  Serial.print("  ");
+  Serial.print("roofsEnd: ");
+  Serial.print(roofEnd);
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 // END FILE
