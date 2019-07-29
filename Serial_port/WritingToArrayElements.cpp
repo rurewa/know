4ed33468fd6ea24d453728731020d35d456ee3e6
Alex <rise.sp@gmail.com>
2019-07-29 21:38:43 +0300

    Update sketch WritingToArrayElements

--- Serial_port/WritingToArrayElements.cpp
+++ Serial_port/WritingToArrayElements.cpp
@@ -6,7 +6,7 @@
 #include <Arduino.h>
 
 const int ARRAY_LENGHT = 5; // Размер массива
-int myNumbers[ARRAY_LENGHT] = { 155, 3, 68, 20, 1 }; // Содержимое массива
+int myNumbers[ARRAY_LENGHT] = { 155, 3, 68, 20, -1 }; // Содержимое массива
 char elementIndex = 0;
 
 void setup() {
@@ -26,11 +26,13 @@ void setup() {
 
 void loop() {
   while (Serial.available() > 0) {
-    elementIndex = Serial.read();
+    elementIndex = Serial.read(); // Ввожу № элемента массива
     Serial.print(" Element: ");
-    Serial.println(elementIndex);
-    Serial.print(" Writing: ");
-    Serial.print(myNumbers[elementIndex]);
+    Serial.println(elementIndex); // Показываю № элемента массива
+    Serial.print(" Extract: ");
+    Serial.print(myNumbers[elementIndex - '0']); // Показываю число элемента массива
+    /*т.е. я ввожу, к примеру 1, что в ASCII равно 49-ми. А ноль в ASCII это 48.
+    Получается 49-48 (elementIndex - '0') = 1. И т.д.  */
   }
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
