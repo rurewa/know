af4574b9f5bf61d459eca359c4f80986721d2af2
rurewa <34348805+rurewa@users.noreply.github.com>
2019-11-25 05:48:17 +0300

    Update DevelopBoardKirill.cpp

--- Arduino_Projects/DevelopBoardKirill/DevelopBoardKirill.cpp
+++ Arduino_Projects/DevelopBoardKirill/DevelopBoardKirill.cpp
@@ -8,12 +8,12 @@ AF_DCMotor Motor1(1); // Объект мотора 1
 AF_DCMotor Motor2(2); // Объект мотора 2
 AF_DCMotor Motor3(3); // Объект мотора 3
 
-int Joy1ValueX = 0, Joy1ValueY= 0; // Джойстик 1 по X и Y
-int Joy2ValueX = 0; // Джойстик 2
+int8_t Joy1ValueX = 0, Joy1ValueY= 0; // Джойстик 1 по X и Y
+int8_t Joy2ValueX = 0; // Джойстик 2
 
 int sigSwhow1 = 9; // Сигнализация
-int sigSwhow2 = 10; // Сигнализация
-int sigSwhow3 = 17; // Сигнализация
+
+void blink();
 
 void setup() {
   Motor1.run(RELEASE); // Останов мотора 1
@@ -25,9 +25,9 @@ void setup() {
 }
 
 void loop() {
-  Joy1ValueX = map (analogRead(A0), 0, 1023, -255, 255); // Преобразование аналогового сигнала в цифровой
-  Joy1ValueY = map (analogRead(A1), 0, 1023, -255, 255);
-  Joy2ValueX = map (analogRead(A2), 0, 1023, -255, 255);
+  Joy1ValueX = map (analogRead(A0), 0, 1023, -127, 127); // Преобразование аналогового сигнала в цифровой
+  Joy1ValueY = map (analogRead(A1), 0, 1023, -127, 127);
+  Joy2ValueX = map (analogRead(A2), 0, 1023, -127, 127);
 
   // Управление первым джойстиком по X и Y
   if (Joy1ValueX >= -10 && Joy1ValueX <= 10)  {
@@ -41,7 +41,8 @@ void loop() {
       Motor1.run(FORWARD); // Крутим мотор 1
       Motor1.setSpeed(Joy1ValueX); // Со скоростью от 11 до 255 по ШИМ
       if (Joy1ValueX > 11) {
-        digitalWrite(sigSwhow1, HIGH); // Сигнализация включена
+        //digitalWrite(sigSwhow1, HIGH); // Сигнализация включена
+        blink();
       }
       //Serial.println(Joy1ValueX);
   }
@@ -49,7 +50,8 @@ void loop() {
       Motor1.run(BACKWARD); // Крутим мотор 1 в обратную сторону
       Motor1.setSpeed(-Joy1ValueX); // Со скоростью от -11 до -255 по ШИМ
       if (Joy1ValueX < -11) {
-        digitalWrite(sigSwhow1, HIGH);
+        //digitalWrite(sigSwhow1, HIGH);
+        blink();
       }
       //Serial.println(Joy1ValueX);
   }
@@ -65,7 +67,8 @@ void loop() {
       Motor2.run(FORWARD);
       Motor2.setSpeed(Joy1ValueY);
       if (Joy1ValueY > 13) {
-        digitalWrite(sigSwhow1, HIGH);
+        //digitalWrite(sigSwhow1, HIGH);
+        blink();
       }
       //Serial.println(Joy1ValueY);
   }
@@ -73,7 +76,8 @@ void loop() {
       Motor2.run(BACKWARD);
       Motor2.setSpeed(-Joy1ValueY);
       if (Joy1ValueY <= -13) {
-        digitalWrite(sigSwhow1, HIGH);
+        //digitalWrite(sigSwhow1, HIGH);
+        blink();
       }
       //Serial.println(Joy1ValueY);
   }
@@ -90,7 +94,8 @@ void loop() {
       Motor3.run(FORWARD);
       Motor3.setSpeed(Joy2ValueX);
       if (Joy2ValueX > 13) {
-        digitalWrite(sigSwhow1, HIGH);
+        //digitalWrite(sigSwhow1, HIGH);
+        blink();
       }
       //Serial.println(Joy2ValueX);
   }
@@ -98,9 +103,19 @@ void loop() {
       Motor3.run(BACKWARD);
       Motor3.setSpeed(-Joy2ValueX);
       if (Joy2ValueX < -13) {
-        digitalWrite(sigSwhow1, HIGH);
+        //digitalWrite(sigSwhow1, HIGH);
+        blink();
       }
       //Serial.println(Joy2ValueX);
+  }
+}
+
+void blink() {
+  // static меняет время жизни переменной на время работы программы
+  static unsigned long blink_time = millis();
+  if (millis() >= blink_time) {  // настало ли время мигнуть
+    digitalWrite(sigSwhow1, !digitalRead(sigSwhow1));  // выключаем/включаем LED на 3,5 сек.
+    blink_time = millis() + 300;      // задаём время следующего переключения
   }
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
