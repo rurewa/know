606d9ed562135e939a56a7b9512728ede6052a61
rurewa <rise.sp@gmail.com>
2020-03-12 05:03:44 +0300

    Update  program Leds Group One Wave 2

--- Arduino_dev_board/Version_2/leds_group_one_wave.cpp
+++ Arduino_dev_board/Version_2/leds_group_one_wave.cpp
@@ -3,32 +3,42 @@
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 #include <Arduino.h>
-
+// Указываем размер массива
 const int ARRAY_SIZE = 5;
+// Объявляем массив элементов
 const int ARRAY_PINS_LEDS[ARRAY_SIZE] = {8, 9, 10, 11, 12};
 
 void setup() {
+  // Настройка пинов в режиме ВЫХОД
   for (int i = 0; i < ARRAY_SIZE; ++i) {
     pinMode(ARRAY_PINS_LEDS[i], OUTPUT);
   }
 }
 
 void loop() {
+  // Состояние текущего светодиода
   static int currentLed = 0;
+  // Шаг
   static int directWave = 1;
-
+  // Выключение всех светодиодов (циклично)
   for (int x = 0; x < ARRAY_SIZE; ++x) {
     digitalWrite(ARRAY_PINS_LEDS[x], LOW);
   }
+  // Включаем текущий светодиод
   digitalWrite(ARRAY_PINS_LEDS[currentLed], HIGH);
-  currentLed += directWave;
+  // Начало движения светодиодной волны
+  currentLed += directWave; // currentLed = currentLed + directWave
+  // Когда currentLed становится 4
   if (currentLed == ARRAY_SIZE -1) {
-    directWave = -directWave;
+    // Начинается обратный отчёт
+    directWave = -directWave; // currentLed += -directWave
   }
+  // Когда отчёт достигает 0
   if (currentLed == 0) {
+    // Возвращаем исходное значение
     directWave = 1;
   }
-  delay(250);
+  delay(300); // Задержка для мигания
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 // END FILE
