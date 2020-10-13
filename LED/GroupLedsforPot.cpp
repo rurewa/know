3e1324e116ad21220ac1d9be6fdb41b4e204c3b0
rurewa <rurewa@gmail.com>
2020-10-13 12:11:09 +0300

    Update program Group Leds For Pot

--- LED/GroupLedsforPot.cpp
+++ LED/GroupLedsforPot.cpp
@@ -1,37 +1,34 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-// Простая светодиодная волна от потенциометра 
+// Простая светодиодная волна от потенциометра
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 #include <Arduino.h>
 
-const int POT= A7; // Аналоговый вход A7 для подключения потенциометра
-// список контактов подключения светодиодной шкалы
 const int PINS_LEDS[5] = {8, 9, 10, 11, 12};
 
 void setup() {
-  for(int i = 0; i <= 4 ; i++) {
-    // Сконфигурировать пины светодиодов как выходы
+  for (int i = 0; i <= 4; i++) {
     pinMode(PINS_LEDS[i], OUTPUT);
-    digitalWrite(PINS_LEDS[i], LOW);
   }
+  Serial.begin(9600);
 }
 
 void loop() {
-  int valPot = 0; // переменная для хранения значения потенциометра
-  int countLeds = 0; // переменная для хранения значения шкалы
-  valPot = analogRead(POT); // чтение данных потенциометра
-  // масштабируем значение к интервалу 0-5
-  countLeds = map(valPot, 0, 1023, 0, 5);
-  // зажигаем светодиоды на шкале, равное countLeds
-  for (int i = 0; i < 5; i++) {
-    if(i < countLeds) { // зажигаем светодиод шкалы
+  const int PIN_POT = A7;
+  int valPot = analogRead(PIN_POT);
+  int countLeds = map (valPot, 0, 1023, 0, 5);
+
+  for (int i = 0; i <= 5; i++) {
+    if (i < countLeds) {
       digitalWrite(PINS_LEDS[i], HIGH);
-  }
-    else { // гасим светодиод шкалы 
+    }
+    else {
       digitalWrite(PINS_LEDS[i], LOW);
     }
   }
+  Serial.println(countLeds);
 }
+
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 // END FILE
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
