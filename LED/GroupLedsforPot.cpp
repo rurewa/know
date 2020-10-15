35f23c10df3711e641c109bcc52b9414607a55a4
rurewa <rurewa@gmail.com>
2020-10-15 11:08:39 +0300

    Update Group Leds For Pot

--- LED/GroupLedsforPot.cpp
+++ LED/GroupLedsforPot.cpp
@@ -3,53 +3,32 @@
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 #include <Arduino.h>
-
-<<<<<<< HEAD
-const int POT= A7; // Аналоговый вход A7 для подключения потенциометра
-// список контактов подключения светодиодной шкалы
+// Определяем группу светодиодов в массиве
 const int PINS_LEDS[5] = {8, 9, 10, 11, 12};
 
 void setup() {
-  for(int i = 0; i <= 4 ; i++) {
-    // Сконфигурировать пины светодиодов как выходы
-=======
-const int PINS_LEDS[5] = {8, 9, 10, 11, 12};
-
-void setup() {
+  // Настраиваем светодиоды как ВЫХОД
   for (int i = 0; i <= 4; i++) {
->>>>>>> master
     pinMode(PINS_LEDS[i], OUTPUT);
   }
-  Serial.begin(9600);
+  Serial.begin(9600); // Для диагностики
 }
 
 void loop() {
-<<<<<<< HEAD
-  int valPot = 0; // переменная для хранения значения потенциометра
-  int countLeds = 0; // переменная для хранения значения шкалы
-  valPot = analogRead(POT); // чтение данных потенциометра
-  // масштабируем значение к интервалу 0-5
-  countLeds = map(valPot, 0, 1023, 0, 5);
-  // зажигаем светодиоды на шкале, равное countLeds
-  for (int i = 0; i < 5; i++) {
-    if(i < countLeds) { // зажигаем светодиод шкалы
-=======
-  const int PIN_POT = A7;
-  int valPot = analogRead(PIN_POT);
-  int countLeds = map (valPot, 0, 1023, 0, 5);
-
-  for (int i = 0; i <= 5; i++) {
-    if (i < countLeds) {
->>>>>>> master
-      digitalWrite(PINS_LEDS[i], HIGH);
+  const int pinPot = A7; // Пин потенциометра
+  int valPot = analogRead(pinPot); // Считываем с потенциометра
+  int countLed = 0; // Для значений текущего числа от 0 до 5
+  countLed = map (valPot, 0, 1023, 0, 5); // Масштабируем значения
+  for (int i = 0; i <= 5; i++) { // Перечисляем от 0 до 5
+    if (i < countLed) { // Если число перечисления < текущего
+      digitalWrite(PINS_LEDS[i], HIGH); // включаем светодиоды
     }
     else {
-      digitalWrite(PINS_LEDS[i], LOW);
+      digitalWrite(PINS_LEDS[i], LOW); // Выключаем светодиоды
     }
   }
-  Serial.println(countLeds);
+  Serial.println(countLed); // Для диагностики
 }
-
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 // END FILE
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
