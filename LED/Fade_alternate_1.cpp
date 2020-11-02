627ef3be013acde64e03f40cb8680ac7b9b5c330
rurewa <rurewa@gmail.com>
2020-11-02 17:29:51 +0300

    Update Fade Amount alternate project

--- LED/Fade_alternate_1.cpp
+++ LED/Fade_alternate_1.cpp
@@ -3,16 +3,16 @@
 // Arduino_dev_board v 2.0 for kids
 // Fade an LED v 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
-const int led = 9;
+const int PIN_LED = 9;
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(PIN_LED, OUTPUT);
 }
 
 void loop() {
   static int brightness = 0; // Начальная яркость
   static int fadeAmount = 5; // Шаг изменения ШИМ
-  analogWrite(led, brightness); // Управление светодиодом
+  analogWrite(PIN_LED, brightness); // Управление светодиодом
   brightness += fadeAmount; // Циклично +5 или -5
   // Когда brightness <= 255, происходит приращение на 5. Когда brightness >=255
   // происходит выполнение brightness += -fadeAmount (-5)
