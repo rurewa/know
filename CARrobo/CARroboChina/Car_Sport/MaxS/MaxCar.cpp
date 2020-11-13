0a8ed3de361aa4e447fecad719458e502eabc63e
rurewa <rurewa@gmail.com>
2020-11-13 13:44:31 +0300

    Added MaxS roboCar project

---
+++ CARrobo/CARroboChina/Car_Sport/MaxS/MaxCar.cpp
@@ -0,0 +1,19 @@
+const int ENA = 3;
+const int IN1 = 1;
+const int IN2 = 5;
+const int IN3 = 9;
+const int IN4 = 7; 
+const int ENB = 8;
+
+void setup() {
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENB, OUTPUT);    
+ }
+
+void loop() {
+   
+}
