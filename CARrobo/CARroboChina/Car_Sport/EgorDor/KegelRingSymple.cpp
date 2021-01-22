851ad6e0e078ce4245fd5cfa45a6d883f5b4f8d4
rurewa <rurewa@gmail.com>
2021-01-22 09:33:06 +0300

    Update Egor cars KegelRingSeple: added black line traking back

--- CARrobo/CARroboChina/Car_Sport/EgorDor/KegelRingSymple.cpp
+++ CARrobo/CARroboChina/Car_Sport/EgorDor/KegelRingSymple.cpp
@@ -25,7 +25,7 @@ const int PIN_ECHO_UP = 17; // 17
 const int PIN_TRIG_UP = 18;  // 18
 
 NewPing sonarDown(PIN_TRIG_DOWN, PIN_ECHO_DOWN, 400);
-NewPing sonarUp(PIN_TRIG_UP, PIN_ECHO_UP, 400);
+NewPing sonarUp(PIN_TRIG_UP, PIN_ECHO_UP, 100);
 
 const int SPEED_LEFT = 225; // Скорость левого мотора
 const int SPEED_RIGHT = 225; // Скорость правого мотора
@@ -37,6 +37,7 @@ void turnLeft(int speed_left, int speed_right, int times);
 void turnRight(int speed_left, int speed_right, int times);
 void turnAngle(int speed_left, int speed_right, int times);
 void sensTest(int times);
+void voiceMove(); // Алгоритм проверки обнаружения перпятствия со звуковым сигналом
 
 const int irSens = 4; // Датчик определения цвета кегли
 
@@ -54,27 +55,50 @@ void setup() {
 }
 
 void loop() {
+  //sensTest(10);
+  bool sLeft = 0; bool sCenter = 0; bool sRight = 0; // Черная 1, белый 0
+  sLeft = digitalRead(SENS_LEFT); sCenter = digitalRead(SENS_CENTER); sRight = digitalRead(SENS_RIGHT);
 
-  // Функция, которая осуществляет цикличный поворот робота влево
-  // и, в случае обнаружения спереди препятствия, даёт звуковой сигнал
-  while (true) {
-    int distance = sonarDown.ping_cm();
-
-    if (distance < 30) { // Если дистанция до кегли меньше 30 см.
-      delay(5); // Немного ждём (для стабилизации)
-      analogWrite(voice, 75); // Включаем сигнал. 0-255 тональность звука
-      Serial.print(distance); // Выводим в Монитор порта расстояние доп репятствия с нижнего сонара (для диагностики)
-      Serial.println(" TurnGo!"); // Отправляем сообщение в Монитор порта (для диагностики)
-      turnStop(100); // Останавливаем движение
-      turnLeft(SPEED_LEFT, SPEED_RIGHT, 50); // Поворачиваем влево
+  if (sLeft == 1 && sCenter == 1 && sRight == 1) { // 111
+    turnBack(SPEED_LEFT, SPEED_RIGHT, 1300);
+    analogWrite(voice, 75); // Включаем сигнал. 0-255 тональность звука
+    sensTest(0);
+    if (sLeft == 1 && sCenter == 1 && sRight == 1) {
+      turnStop(2000);
+      turnBack(SPEED_LEFT, SPEED_RIGHT, 250);
     }
-    else { // Если расстояние до ближайшей кегли больше 30 сантиметров, то
-      delay(5); // Немного ждём (для стабилизации)
-      analogWrite(voice, 0); // Выключаем сигнал.
-      Serial.print(distance); // Выводим в Монитор порта расстояние доп репятствия с нижнего сонара (для диагностики)
-      Serial.println(" TurnLeft!"); // Отправляем сообщение в Монитор порта (для диагностики)
-      turnLeft(SPEED_LEFT, SPEED_RIGHT, 5); // Поворачиваем влево
+  }
+  else if (sLeft == 1 && sCenter == 0 && sRight == 0) { // 100. Сделать короткий поворот вправо
+    turnBack(SPEED_LEFT, SPEED_RIGHT, 1300);
+    analogWrite(voice, 75); // Включаем сигнал. 0-255 тональность звука
+    sensTest(0);
+    if (sLeft == 1 && sCenter == 0 && sRight == 0) {
+      turnStop(2000);
+      turnBack(SPEED_LEFT, SPEED_RIGHT, 250);
     }
+  }
+  else if (sLeft == 0 && sCenter == 1 && sRight == 0) { // 010
+    turnBack(SPEED_LEFT, SPEED_RIGHT, 1300);
+    analogWrite(voice, 75); // Включаем сигнал. 0-255 тональность звука
+    sensTest(0);
+    if (sLeft == 0 && sCenter == 1 && sRight == 0) {
+      turnStop(2000);
+      turnBack(SPEED_LEFT, SPEED_RIGHT, 250);
+    }
+  }
+  else if (sLeft == 0 && sCenter == 0 && sRight == 1) { // 001. Сделать короткий поворот влево
+    turnBack(SPEED_LEFT, SPEED_RIGHT, 1300);
+    analogWrite(voice, 75); // Включаем сигнал. 0-255 тональность звука
+    sensTest(0);
+    if (sLeft == 0 && sCenter == 0 && sRight == 1) {
+      turnStop(2000);
+      turnBack(SPEED_LEFT, SPEED_RIGHT, 250);
+    }
+  }
+  else {
+    turnGo(SPEED_LEFT, SPEED_RIGHT, 50); // 000
+    analogWrite(voice, 0); // Включаем сигнал. 0-255 тональность звука
+    sensTest(0);
   }
 }
 
@@ -145,6 +169,28 @@ void sensTest(int times) {
   Serial.print("Right: ");
   Serial.println(sRight = digitalRead(SENS_RIGHT));
   delay(times);
+}
+
+void voiceMove() {
+  // Функция, которая осуществляет цикличный поворот робота влево
+  // и, в случае обнаружения спереди препятствия, даёт звуковой сигнал
+  int distance = sonarDown.ping_cm();
+
+  if (distance < 25) { // Если дистанция до кегли меньше 30 см.
+    //delay(5); // Немного ждём (для стабилизации)
+    analogWrite(voice, 75); // Включаем сигнал. 0-255 тональность звука
+    //Serial.print(distance); // Выводим в Монитор порта расстояние доп репятствия с нижнего сонара (для диагностики)
+    //Serial.println(" TurnGo!"); // Отправляем сообщение в Монитор порта (для диагностики)
+    turnStop(350); // Останавливаем движение
+    turnLeft(SPEED_LEFT, SPEED_RIGHT, 100); // Поворачиваем влево
+  }
+  else { // Если расстояние до ближайшей кегли больше 30 сантиметров, то
+    delay(5); // Немного ждём (для стабилизации)
+    analogWrite(voice, 0); // Выключаем сигнал.
+    Serial.print(distance); // Выводим в Монитор порта расстояние доп репятствия с нижнего сонара (для диагностики)
+    Serial.println(" TurnLeft!"); // Отправляем сообщение в Монитор порта (для диагностики)
+    turnLeft(SPEED_LEFT, SPEED_RIGHT, 5); // Поворачиваем влево
+  }
 }
 
 /* // Диагностика нижнего сонара
