22d3c130c3f2165421da1aa6d5e147a69c3b34d5
Alex <rise.sp@gmail.com>
2021-04-20 11:08:14 +0300

    Update MenuLCDdisplyThreeButtons project

--- Arduino_dev_board/Version_2/MenuLCDdisplyThreeButtons.cpp
+++ Arduino_dev_board/Version_2/MenuLCDdisplyThreeButtons.cpp
@@ -1,7 +1,6 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-// Простой алгоритм счётчика нажатий 2-х кнопок. Перелистывание пунсктов меню на дисплее
-// Известные проблемы: счётчик считает от 0 и не попадает в индекс массива. Накладывются
-// друг на друга слова
+// Простой алгоритм счётчика нажатий 3-х кнопок. Перелистывание пунсктов меню на дисплее
+// Известные проблемы: не стабильно работает вход в пунст меню
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 #include <Arduino.h>
@@ -10,9 +9,9 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Устанавливаем дисплей
 
-Button_Simple button13(13); // Пин кнопки
-Button_Simple button16(16); // Пин кнопки
-Button_Simple button17(17); // Пин кнопки
+Button_Simple button13(13); // Пин кнопки перелистывания вверз
+Button_Simple button16(16); // Пин кнопки входа в меню
+Button_Simple button17(17); // Пин кнопки перелистывания вниз
 
 // Верхний и нижний лимиты счётчика
 const int LIMIT_UP = 4;
@@ -47,55 +46,92 @@ void loop() {
     if (++click13 >= 2) { click13 = 0; --COUNT; } // Счётчик с ограничителем (0/1)
     if (click13 == 1) { --COUNT; }
   }
-  // Пока не знаю как мне войти в меню по нажатию кнопки А2
   while (button16.clicked()) {
-    if (++click16 >= 2) { click13 = 0; } // Счётчик с ограничителем (0/1)
-    if (click13 == 1) {  }
+    if (++click16 >= 2) { click16 = 0; } // Счётчик с ограничителем (0/1)
   }
   while (COUNT >= LIMIT_UP) { COUNT = LIMIT_UP; break; } // Ограничение инкремента
   while (COUNT <= LIMIT_DOWN) { COUNT = LIMIT_DOWN; break;  } // Ограничение декремента
-  Serial.println(COUNT); // Для дианостики
   switch (COUNT)
   {
   case 0:
-    lcd.clear(); // Очищаем экран перед получением нового значения
     lcd.setCursor(2, 0);      // курсор на 4-й символ 1-й строки
     lcd.print("Choose menu"); // Тест на 1-й строке экрана
     lcd.setCursor(0, 1);  // курсор/строка
     lcd.print("Menu Temperature:"); // Тест на 2-й строке экрана
+    if (click16 == 1) {
+      lcd.setCursor(0, 0);      // курсор/строка
+      lcd.print("Curient t C     "); // Тест на 1-й строке экрана
+      lcd.setCursor(0, 1);
+      lcd.print("      30 C      ");
+      delay(100);
+    }
+    else { break; }
     break;
   case 1:
-    lcd.clear(); // Очищаем экран перед получением нового значения
     lcd.setCursor(2, 0);      // курсор на 4-й символ 1-й строки
     lcd.print("Choose menu"); // Тест на 1-й строке экрана
     lcd.setCursor(0, 1);
-    lcd.print("Menu Motor:");
+    lcd.print("Menu Motor:     ");
+    if (click16 == 1) {
+      lcd.setCursor(0, 0);      // курсор/строка
+      lcd.print("    Motor pwm   "); // Тест на 1-й строке экрана
+      lcd.setCursor(0, 1);
+      lcd.print("    155 PWM     ");
+      delay(100);
+    }
+    else { break; }
     break;
   case 2:
     lcd.setCursor(2, 0);      // курсор на 4-й символ 1-й строки
     lcd.print("Choose menu"); // Тест на 1-й строке экрана
     lcd.setCursor(0, 1);
-    lcd.print("Menu Voice:");
+    lcd.print("Menu Voice:     ");
+    if (click16 == 1) {
+      lcd.setCursor(0, 0);      // курсор/строка
+      lcd.print("   Voice beep   "); // Тест на 1-й строке экрана
+      lcd.setCursor(0, 1);
+      lcd.print("      beep      ");
+      delay(100);
+    }
+    else { break; }
     break;
   case 3:
     lcd.setCursor(2, 0);      // курсор на 4-й символ 1-й строки
     lcd.print("Choose menu"); // Тест на 1-й строке экрана
     lcd.setCursor(0, 1);
-    lcd.print("Menu 8x8 led matrix:");
+    lcd.print("8x8 led matrix:");
+    if (click16 == 1) {
+      lcd.setCursor(0, 0);      // курсор/строка
+      lcd.print("      Matrix    "); // Тест на 1-й строке экрана
+      lcd.setCursor(0, 1);
+      lcd.print("     Start      ");
+      delay(100);
+    }
+    else { break; }
     break;
   case 4:
     lcd.setCursor(2, 0);      // курсор на 4-й символ 1-й строки
     lcd.print("Choose menu"); // Тест на 1-й строке экрана
     lcd.setCursor(0, 1);
-    lcd.print("Menu Sonar:");
+    lcd.print("Menu Sonar:     ");
+    if (click16 == 1) {
+      lcd.setCursor(0, 0);      // курсор/строка
+      lcd.print("      Sonar     "); // Тест на 1-й строке экрана
+      lcd.setCursor(0, 1);
+      lcd.print("distance:       ");
+      delay(100);
+    }
+    else { break; }
     break;
   default:
     lcd.setCursor(2, 0);      // курсор на 4-й символ 1-й строки
     lcd.print("Choose menu"); // Тест на 1-й строке экрана
     lcd.setCursor(0, 1);
-    lcd.print("Menu 1:");
+    lcd.print("Menu 1:         ");
     break;
   }
+  // Пока не знаю как мне войти в меню по нажатию кнопки А2
+  //Serial.println(COUNT); // Для дианостики
 }
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
