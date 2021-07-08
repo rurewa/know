2310e50b55ed50984fa53a349f065af11e9a14b7
rurewa <rurewa@gmail.com>
2021-07-08 13:31:20 +0300

    Add NRF24L01 Tanl_2WD project

---
+++ CARrobo/RadioControl/NRF2401/Tank_2WD/NRF24L01/tx/tx.ino
@@ -0,0 +1,34 @@
+#include "nRF24L01.h"
+#include "RF24.h"
+
+// transmitter
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+char xyData[32] = "";
+String xAxis, yAxis;
+
+const char text[] = "Hello World"; // Диагностика
+
+void setup() 
+{
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  radio.write(&text, sizeof(text));
+}
+
+void loop() 
+{
+  
+  xAxis = analogRead(A0); // Считываем показания по X
+  yAxis = analogRead(A1); // Считываем показания по Y
+  // Значение по Х
+  xAxis.toCharArray(xyData, 5); // Переводим значения X в массив символов
+  radio.write(&xyData, sizeof(xyData)); // Отправляем данные массива X в другой модуль NRF24L01
+  // Значение по Y
+  yAxis.toCharArray(xyData, 5);
+  radio.write(&xyData, sizeof(xyData));
+  delay(20);
+}
