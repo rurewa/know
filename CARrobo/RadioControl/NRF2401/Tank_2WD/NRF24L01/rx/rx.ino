05c5e77b1de3e69db459c8f593adecebf066d285
rurewa <rurewa@gmail.com>
2021-07-08 14:05:36 +0300

    Update Tank_2WD/NRF24L01: remake control

--- CARrobo/RadioControl/NRF2401/Tank_2WD/NRF24L01/rx/rx.ino
+++ CARrobo/RadioControl/NRF2401/Tank_2WD/NRF24L01/rx/rx.ino
@@ -1,3 +1,7 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
+// RX. Tank 2WD whitch NRF24L01.
+// V 1.0
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 #include "nRF24L01.h"
 #include "RF24.h"
 // receiver
@@ -73,47 +77,31 @@ void loop()
   // X-axis used for left and right control
   if (xAxis < 470) 
   {
-    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
-    int xMapped = map(xAxis, 470, 0, 0, 255);
-    // Move to left - decrease left motor speed, increase right motor speed
-    motorSpeedA = motorSpeedA - xMapped;
-    motorSpeedB = motorSpeedB + xMapped;
-    // Confine the range from 0 to 255
-    if (motorSpeedA < 0) 
-    {
-      motorSpeedA = 0;
-    }
-    if (motorSpeedB > 255) 
-    {
-      motorSpeedB = 255;
-    }
+    // Set Motor A backward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // Set Motor B backward
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    // Convert the declining X-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
+    motorSpeedA = map(xAxis, 470, 0, 0, 255);
+    motorSpeedB = map(xAxis, 470, 0, 0, 255);
   }
   if (xAxis > 550) 
   {
-    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
-    int xMapped = map(xAxis, 550, 1023, 0, 255);
-    // Move right - decrease right motor speed, increase left motor speed
-    motorSpeedA = motorSpeedA + xMapped;
-    motorSpeedB = motorSpeedB - xMapped;
-    // Confine the range from 0 to 255
-    if (motorSpeedA > 255) 
-    {
-      motorSpeedA = 255;
-    }
-    if (motorSpeedB < 0) 
-    {
-      motorSpeedB = 0;
-    }
-  }
-  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-  if (motorSpeedA < 70) 
-  {
-    motorSpeedA = 0;
-  }
-  if (motorSpeedB < 70) 
-  {
-    motorSpeedB = 0;
+    // Set Motor A forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    // Set Motor B forward
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    // Convert the increasing X-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+    motorSpeedA = map(xAxis, 550, 1023, 0, 255);
+    motorSpeedB = map(xAxis, 550, 1023, 0, 255);
   }
   analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
   analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
 }
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
+// END FILE
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
